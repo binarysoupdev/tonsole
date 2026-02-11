3955652c3917fa96c72a561e7adee0e30d3e9833
binary_soup <contact@binarysoup.dev>
2026-02-10 21:20:32 -0500

    Update docs

--- testio/notify.go
+++ testio/notify.go
@@ -2,10 +2,10 @@ package testio
 
 var signal = make(chan struct{})
 
-// Notify the go routine to queue the next input.
+// Notify the goroutine to queue the next input.
 // Should be called right before reading from stdin.
 //
-// If the StdinPipe is not open, this function does nothing.
+// Note: if the StdinPipe is not open, this function does nothing.
 func Notify() {
 	if input != nil {
 		signal <- struct{}{}
