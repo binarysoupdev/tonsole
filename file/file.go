303f13392167b6e8a054c5109174c5df3d275f87
binary_soup <contact@binarysoup.dev>
2026-02-10 22:00:20 -0500

    Create test file helper package + tests

---
+++ file/file.go
@@ -0,0 +1,32 @@
+package file
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+// Create a new test directory and it join with path.
+func NewPath(t *testing.T, path string) string {
+	return filepath.Join(t.TempDir(), path)
+}
+
+// Create an empty file in a new test directory and return its path.
+func CreateEmpty(t *testing.T, path string) string {
+	file, path := Create(t, path)
+	file.Close()
+
+	return path
+}
+
+// Create a new file in a new test directory. Returns the open file pointer and the path.
+func Create(t *testing.T, path string) (*os.File, string) {
+	path = NewPath(t, path)
+
+	file, err := os.Create(path)
+	require.NoError(t, err)
+
+	return file, path
+}
---
+++ file/file_test.go
@@ -0,0 +1,27 @@
+package file_test
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/binarysoupdev/tinsel/file"
+	"github.com/binarysoupdev/tinsel/rand"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreateFile(t *testing.T) {
+	//-- arrange
+	r := rand.New(64)
+	FILE := fmt.Sprintf("%s.%s", r.ASCII(10), r.ASCII(3))
+
+	//-- act
+	path := file.CreateEmpty(t, FILE)
+
+	//-- assert
+	stat, err := os.Stat(path)
+
+	require.NoError(t, err)
+	assert.Equal(t, FILE, stat.Name())
+}
