00718ec390b0592dceee25b2f641db0a6ba03511
binary_soup <contact@binarysoup.dev>
2026-02-19 19:35:41 -0500

    Fix typo

--- doc.go
+++ doc.go
@@ -1,5 +1,5 @@
 /*
-The tinsel modules provides several packages to assist in testing console-based applications.
+The tinsel module provides several packages to assist in testing console-based applications.
 
 The [github.com/binarysoupdev/tinsel/pipe] package provides the StdioPipe for reading/writing from stdin/stdout.
 
